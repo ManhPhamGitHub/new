41672ef8e08f55dde587bbee2c4ad1e1943cb765
manh2001827 <66905530+manh2001827@users.noreply.github.com>
2022-02-09 17:08:16 +0700

    test

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ mongoose.connect(`mongodb+srv://manh278:manh278@cluster0.3uwla.mongodb.net/myFir
   console.log(err);
 });
 
-app.use(cors({}))
+app.use(cors({origin:'*', 
+credentials:true,            //access-control-allow-credentials:true
+optionSuccessStatus:200,}))
 app.use(bodyParser.json());
 
 // 
